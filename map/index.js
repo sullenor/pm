22b0c0adb6791d9f62e87e64c8d76559bba341dd
Alexey Litvinov <sullenor@yandex-team.ru>
2016-09-22 10:03:58 +0300

    map update

--- map/index.js
+++ map/index.js
@@ -9,14 +9,26 @@ new Suite()
   .on('cycle', event => console.log(String(event.target)))
   .add('[].forEach', () => collection.map(identity))
   .add('_.forEach', () => map(collection, identity))
+  .add('for-in-loop', () => forIn(collection, identity))
   .add('improved native for-loop', () => nativeForLoop(collection, identity))
   .run();
 
 function nativeForLoop(collection, iteratee) {
-  const nextCollection = new Array(collection.length);
+  const nextCollection = Array(collection.length);
 
   for (var i = collection.length - 1; i >= 0; i--) {
-    nextCollection[i] = identity(collection[i]);
+    nextCollection[i] = iteratee(collection[i]);
+  }
+
+  return nextCollection;
+}
+
+function forIn(collection, iteratee) {
+  const nextCollection = Array();
+
+  for (var key in collection) {
+    if (!collection.hasOwnProperty(key)) continue;
+    nextCollection[key] = iteratee(collection[key]);
   }
 
   return nextCollection;
--- map/readme.md
+++ map/readme.md
@@ -1,7 +1,8 @@
 # map
 
 ```bash
-[].forEach x 10,058 ops/sec ±1.16% (88 runs sampled)
-_.forEach x 136,602 ops/sec ±0.97% (90 runs sampled)
-improved native for-loop x 902,713 ops/sec ±1.05% (83 runs sampled)
+[].forEach x 3,953 ops/sec ±1.65% (87 runs sampled)
+_.forEach x 123,471 ops/sec ±2.19% (85 runs sampled)
+for-in-loop x 7,508 ops/sec ±1.63% (88 runs sampled)
+improved native for-loop x 816,517 ops/sec ±2.38% (87 runs sampled)
 ```
